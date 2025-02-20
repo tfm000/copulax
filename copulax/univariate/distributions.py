263592302081e8fc9f6db91d6d9bdd5410afcd26
tfm000 <tylerfmitchell1999@gmail.com>
2025-02-20 17:49:56 +0000

    updating imports

--- copulax/univariate/distributions.py
+++ copulax/univariate/distributions.py
@@ -6,16 +6,30 @@ Currently the following distribution collections are implemented:
 - continuous: contains all continuous distributions.
 - discrete: contains all discrete distributions.
 """
-from copulax.univariate import gamma, gh, gig, ig, lognormal, normal, skewed_t, student_t, uniform
+# importing pytree distribution objects
+from copulax._src.univariate.gamma import gamma
+from copulax._src.univariate.gh import gh
+from copulax._src.univariate.gig import gig
+from copulax._src.univariate.ig import ig
+from copulax._src.univariate.lognormal import lognormal
+from copulax._src.univariate.normal import normal
+from copulax._src.univariate.skewed_t import skewed_t
+from copulax._src.univariate.student_t import student_t
+from copulax._src.univariate.uniform import uniform
+
+# importing categorizing map
+from copulax._src._distributions import DistMap
 
 __dists = (gamma, gh, gig, ig, lognormal, normal, skewed_t, student_t, uniform)
-__common_names = ('gamma', 'normal', 'student_t', 'uniform', 'lognormal',)
+__common_names = ('Gamma', 'Normal', 'Student-T', 'Uniform', 'LogNormal',)
 
 _all_dists: list = []
 _dist_tree = {'continuous': {}, 'discrete': {}}
 _dist_tree['common'] = _dist_tree.copy()
+_all_dist_objects: list = []
 for dist in __dists:
     _all_dists.append(dist.name)
+    _all_dist_objects.append(DistMap.name_map[dist.name]['object_name'])
 
     _dist_tree[dist.name] = dist
 
@@ -31,4 +45,4 @@ continuous: dict = distributions['continuous'].copy()
 discrete: dict = distributions['discrete'].copy()
 
 __all__ = ['distributions', 'common', 'continuous', 'discrete']
-__all__ += _all_dists
+__all__ += _all_dist_objects
