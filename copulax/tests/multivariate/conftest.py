072dcf68851d32a111371309e9fb4a2c6bfe664f
tfm000 <tylerfmitchell1999@gmail.com>
2025-03-03 18:29:13 +0000

    implementing correlation and covariance estimators

---
+++ copulax/tests/multivariate/conftest.py
@@ -0,0 +1,50 @@
+import pytest
+import numpy as np
+
+
+NUM_SAMPLES: int = 100
+NUM_ASSETS: int = 5
+
+
+np.random.seed(0)
+
+
+def gen_uncorrelated_data(num_samples: int, num_assets: int):
+    return np.random.normal(size=(num_samples, num_assets))
+
+
+def gen_correlated_data(num_assets: int, num_samples: int):
+    # generating a random correlation matrix
+    random_uniform: np.ndarray = np.random.uniform(-1, 1, size=(num_assets, num_assets))
+    lower_triangular: np.ndarray = np.tril(random_uniform)
+    correlation: np.ndarray = lower_triangular + lower_triangular.T
+    np.fill_diagonal(correlation, 1.0)
+
+    # converting to covariance matrix
+    sigma_diag: np.ndarray = np.diag(np.random.uniform(1, 4, num_assets))
+    covariance_matrix: np.ndarray = correlation @ sigma_diag @ correlation.T
+
+    # generating correlated data
+    return np.random.multivariate_normal(np.zeros(num_assets), covariance_matrix, num_samples)
+
+
+@pytest.fixture(scope='package', autouse=True)
+def uncorrelated_sample():
+    return gen_uncorrelated_data(NUM_SAMPLES, NUM_ASSETS)
+
+
+@pytest.fixture(scope='package', autouse=True)
+def correlated_sample():
+    return gen_correlated_data(NUM_ASSETS, NUM_SAMPLES)
+
+
+@pytest.fixture(scope='package', autouse=True)
+def uncorrelated_small_sample():
+    return gen_uncorrelated_data(NUM_ASSETS, NUM_ASSETS)
+
+
+@pytest.fixture(scope='package', autouse=True)
+def correlated_small_sample():
+    return gen_correlated_data(NUM_ASSETS+1, NUM_ASSETS)
+
+
