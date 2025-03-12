f315cb250bf7c2c3a7b339895ec4fcec7a9c45c5
tfm000 <tylerfmitchell1999@gmail.com>
2025-03-12 12:38:37 +0000

    assert causes jit error

--- copulax/_src/multivariate/_utils.py
+++ copulax/_src/multivariate/_utils.py
@@ -7,6 +7,5 @@ def _multivariate_input(x: ArrayLike, dtype=float) -> tuple[jnp.ndarray, tuple[i
     """Ensures all input arrays are of the same dtype and (n, d) shape."""
     x_arr: jnp.array = jnp.asarray(x, dtype=dtype)
     xshape = x_arr.shape
-    assert len(xshape) == 2, "Input array must have shape (n, d)."
     n, d = xshape
     return x_arr.reshape((n, d)), (n, 1), n, d
