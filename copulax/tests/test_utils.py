d44dcc1781b5f2a511118cfbb434285384e5f344
tfm000 <tylerfmitchell1999@gmail.com>
2025-03-27 13:53:41 +0000

    upgrading random key generators

---
+++ copulax/tests/test_utils.py
@@ -0,0 +1,21 @@
+from jax._src.prng import PRNGKeyArray
+
+from copulax import get_random_key, get_api_random_key, get_local_random_key
+
+def _check_valid_key(key, s: str) -> None:
+    """Checks if the key is a valid JAX PRNGKey."""
+    assert isinstance(key, PRNGKeyArray), f"{s} key is not a valid JAX PRNGKey."
+
+def test_get_random_key():
+    """Tests the get_random_key function."""
+    # testing api method
+    api_key = get_api_random_key(max_attempts=10)
+    _check_valid_key(api_key, "API")
+    wrapper_api_key = get_random_key(local=False, max_attempts=10)
+    _check_valid_key(wrapper_api_key, "Wrapper API")
+
+    # testing local method
+    local_key = get_local_random_key(max_attempts=0)
+    _check_valid_key(local_key, "Local")
+    wrapper_local_key = get_random_key(local=True, max_attempts=0)
+    _check_valid_key(wrapper_local_key, "Wrapper Local")
