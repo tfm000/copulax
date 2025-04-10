5f617a5502c240e526525d4ca870929127c1b2ae
tfm000 <tylerfmitchell1999@gmail.com>
2025-04-10 15:59:43 +0100

    cubic approximation to ppf

--- copulax/_src/univariate/_ppf.py
+++ copulax/_src/univariate/_ppf.py
@@ -4,45 +4,83 @@ from jax._src.typing import ArrayLike, Array
 import jax
 import jax.numpy as jnp
 from typing import Callable
+from interpax import Interpolator1D
 
 from copulax._src.optimize import projected_gradient
 from copulax._src.univariate._utils import _univariate_input
+from copulax._src.typing import Scalar
 
 
-def _ppf(dist, q: ArrayLike, params: dict, x0: float) -> Array:
-    q, qshape = _univariate_input(q)
-    x0 = jnp.asarray(x0, dtype=float).reshape((1,))
-
-    # def _ppf_func_single(xi: float, qi: float):
-    #     return jnp.abs((dist.cdf(x=xi, params=params) - qi)).reshape(())
+def _ppf_optimizer(dist, q: ArrayLike, params: dict, x0: Scalar, 
+                   bounds: jnp.ndarray, lr: float, maxiter: int) -> Array:
     
     def _ppf_func_single(xi: float, qi: float):
-        return jnp.abs((xi**2 - qi)).reshape(())  # TODO: find why gradient isnt flowing through projected gradient
+        return jnp.abs((dist.cdf(x=xi, params=params) - qi)).reshape(())
     
-    bounds = dist.support(params)
     min_val, max_val = bounds.reshape((2,1))
     SCALE = 0.5
     x0_small = jnp.max(jnp.array([x0 * (1-SCALE), min_val]))
     x0_large = jnp.min(jnp.array([x0 * (1+SCALE), max_val]))
     def _iter(carry, qi):
-        x0 = jnp.where(qi <= 0.5, x0_small, x0_large)
+        x0 = jnp.where(qi <= 0.5, x0_small, x0_large).reshape((1,))
         res = projected_gradient(
-            f=_ppf_func_single, x0=x0, lr=0.01, 
+            f=_ppf_func_single, x0=x0, lr=lr, maxiter=maxiter, 
             projection_method='projection_box', 
             projection_options={'hyperparams': bounds}, qi=qi)
         return carry, res['x']
 
     _, x = jax.lax.scan(_iter, None, q)
+    return x.flatten()
 
-    # x = jnp.where(jnp.logical_and(x >= bounds[0], x <= bounds[1]), x, jnp.nan)
-    # x = jnp.where(q == 0, bounds[0], x)
-    # x = jnp.where(q == 1, bounds[1], x)
-    return x.reshape(qshape)
+
+def _cubic_ppf(dist, q: ArrayLike, params: dict, x0: Scalar, 
+                bounds: jnp.ndarray, num_points: int,  lr: float, 
+                maxiter: int) -> Array:
+    # ensuring q has at least 3 elements
+    if q.size < 3:
+        # more efficient to use ppf_optimiser
+        return _ppf(dist=dist, q=q, params=params, x0=x0, cubic=False, 
+                    lr=lr, maxiter=maxiter)
+
+    # clip q to (eps, 1-eps) to avoid numerical issues near bounds
+    eps: float = 1e-5
+    q_clipped: jnp.ndarray = jnp.clip(q, eps, 1-eps)
+
+    # getting interpolation bounds
+    q_range: jnp.ndarray = jnp.array([jnp.min(q_clipped), jnp.max(q_clipped)]
+                                     ).reshape((2, 1))
+    x_min, x_max = _ppf_optimizer(dist=dist, q=q_range, params=params, x0=x0, 
+                                  bounds=bounds, lr=lr, maxiter=maxiter)
+
+    # getting interpolation points
+    x_range: jnp.ndarray = jnp.linspace(x_min, x_max, num_points).flatten()  
+    cdf_values: jnp.ndarray = dist.cdf(x=x_range, params=params)
+
+    # interpolating
+    interpolator = Interpolator1D(x=cdf_values, f=x_range, method='monotonic', extrap=True)
+    interpolated_clipped_values: jnp.ndarray = interpolator(q_clipped)
+    interpolated_values: jnp.ndarray = jnp.where(q <= eps, bounds[0], interpolated_clipped_values)
+    interpolated_values = jnp.where(q >= 1-eps, bounds[1], interpolated_values)
+    return interpolated_values
     
 
-# TODO: implement an approx ppf which interpolates between 100 quantiles
-# for a given distribution. This is useful as the ppf func can be slow 
-# when the analytical cdf unknown and can be infeasible to use when 
-# large sample sizes. This will be useful for the copula ppf -> have a 
-# approx_ppf arg in the get_x_dash func and others that use it. 
-# have default set to false. use iterpax for this.
+
+def _ppf(dist, x0: Scalar, q: ArrayLike, params: dict, cubic: bool, 
+         num_points: int, lr: float, maxiter: int) -> Array:
+    q, qshape = _univariate_input(q)
+    q = q.flatten()
+    x0 = jnp.asarray(x0, dtype=float).reshape((1,))
+    bounds = dist.support(params)
+
+    if cubic:
+        x: jnp.ndarray = _cubic_ppf(dist=dist, q=q, params=params, x0=x0, 
+                                     bounds=bounds, num_points=num_points, 
+                                     lr=lr, maxiter=maxiter)
+    else:
+        x: jnp.ndarray = _ppf_optimizer(dist=dist, q=q, params=params, x0=x0, 
+                                        bounds=bounds, lr=lr, maxiter=maxiter)
+        
+    x = jnp.where(jnp.logical_and(x >= bounds[0], x <= bounds[1]), x, jnp.nan)
+    x = jnp.where(q == 0, bounds[0], x)
+    x = jnp.where(q == 1, bounds[1], x)
+    return x.reshape(qshape)
