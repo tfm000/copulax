d8906ecd2b2d339918ff207e5401696a41fd7501
tfm000 <tylerfmitchell1999@gmail.com>
2025-04-15 16:29:05 +0100

    updating ig

--- copulax/_src/univariate/ig.py
+++ copulax/_src/univariate/ig.py
@@ -26,84 +26,77 @@ class IG(Univariate):
         alpha, beta = self._args_transform(alpha, beta)
         return {"alpha": alpha, "beta": beta}
     
+    def _params_to_tuple(self, params):
+        params = self._args_transform(params)
+        return params["alpha"], params["beta"]
+    
+    def example_params(self, *args, **kwargs) -> dict:
+        r"""Example parameters for the inverse gamma distribution.
+        
+        This is a two parameter family, with the inverse gamma being 
+        defined by parameters `alpha` and `beta`.
+        """
+        return self._params_dict(alpha=1.0, beta=1.0)
+
     def support(self, *args, **kwargs) -> Array:
         return jnp.array([0.0, jnp.inf])
     
-    def _stable_logpdf(self, stability: Scalar, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
+    def _stable_logpdf(self, stability: Scalar, x: ArrayLike, params: dict) -> Array:
         x, xshape = _univariate_input(x)
-        alpha, beta = self._args_transform(alpha, beta)
+        alpha, beta = self._params_to_tuple(params)
         
-        logpdf: jnp.ndarray = alpha * jnp.log(beta + stability) - lax.lgamma(alpha) - (alpha + 1) * jnp.log(x) - (beta / x)
+        logpdf: jnp.ndarray = (alpha * jnp.log(beta + stability) 
+                               - lax.lgamma(alpha) 
+                               - (alpha + 1) * jnp.log(x) 
+                               - (beta / x))
         return logpdf.reshape(xshape)
-    
-    def logpdf(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        return super().logpdf(x=x, alpha=alpha, beta=beta)
-    
-    def pdf(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        return super().pdf(x=x, alpha=alpha, beta=beta)
-    
-    def logcdf(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        return super().logcdf(x=x, alpha=alpha, beta=beta)
 
-    def cdf(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
+    def cdf(self, x: ArrayLike, params: dict) -> Array:
         x, xshape = _univariate_input(x)
-        alpha, beta = self._args_transform(alpha, beta)
+        alpha, beta = self._params_to_tuple(params)
         cdf: jnp.ndarray = scipy.special.gammaincc(a=alpha, x=(beta / x))
         return cdf.reshape(xshape)
     
-    def ppf(self, q: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        q, qshape = _univariate_input(q)
-        alpha, beta = self._args_transform(alpha, beta)
-        ppf: jnp.ndarray = beta / tfp.math.igammacinv(a=alpha, p=q)
-        return ppf.reshape(qshape)
-    
-    def inverse_cdf(self, q: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        return super().inverse_cdf(q=q, alpha=alpha, beta=beta)
+    # ppf
+    def _get_x0(self, params):
+        return self.stats(params=params)['mode']
+
+    def _ppf(self, q: ArrayLike, params: dict, *args, **kwargs) -> Array:
+        alpha, beta = self._params_to_tuple(params)
+        return beta / tfp.math.igammacinv(a=alpha, p=q)
     
     # sampling
-    def rvs(self, size: tuple | Scalar=(), key: Array = DEFAULT_RANDOM_KEY, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        alpha, beta = self._args_transform(alpha, beta)
-        return 1.0 / gamma.rvs(size=size, key=key, alpha=alpha, beta=beta)
-    
-    def sample(self, size: tuple | Scalar=(), key: Array = DEFAULT_RANDOM_KEY, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Array:
-        return super().sample(size=size, key=key, alpha=alpha, beta=beta)
+    def rvs(self, size: tuple | Scalar, params: dict, key: Array = DEFAULT_RANDOM_KEY) -> Array:
+        return 1.0 / gamma.rvs(size=size, key=key, params=params)
     
     # stats
     def stats(self, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> dict:
         alpha, beta = self._args_transform(alpha, beta)
-
         mean: float = jnp.where(alpha > 1.0, beta / (alpha - 1), jnp.nan)
         mode: float = beta / (alpha + 1)
         variance: float = jnp.where(alpha > 2.0, lax.pow(beta, 2) / (lax.pow(alpha - 1, 2) * (alpha - 1)), jnp.nan)
+        std: float = jnp.sqrt(variance)
         skewness: float = jnp.where(alpha > 3.0, 4 * jnp.sqrt(alpha - 2) / (alpha - 3), jnp.nan)
         kurtosis: float = jnp.where(alpha > 4.0, 6 * (5*alpha - 11) / ((alpha - 3) * (alpha - 4)), jnp.nan)
-        return {"mean": mean, "mode": mode, "variance": variance, "skewness": skewness, "kurtosis": kurtosis}
-    
-    # metrics
-    def loglikelihood(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Scalar:
-        return super().loglikelihood(x, alpha=alpha, beta=beta)
-    
-    def aic(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Scalar:
-        return super().aic(x, alpha=alpha, beta=beta)
-    
-    def bic(self, x: ArrayLike, alpha: Scalar = 1.0, beta: Scalar = 1.0) -> Scalar:
-        return super().bic(x, alpha=alpha, beta=beta)
+        return {"mean": mean, "mode": mode, "variance": variance, "std": std, 
+                "skewness": skewness, "kurtosis": kurtosis}
     
     # fitting
-    def _fit_mle(self, x: ArrayLike, *args, **kwargs) -> dict:
+    def _fit_mle(self, x: ArrayLike, lr: float, maxiter: int) -> dict:
         key1, key2 = random.split(DEFAULT_RANDOM_KEY)
         params0: jnp.ndarray = jnp.array([gamma.rvs(size=(), key=key1), 
                                         gamma.rvs(size=(), key=key2)])
         
-        res = projected_gradient(f=self._mle_objective, x0=params0, 
-                                projection_method='projection_non_negative', x=x)
+        res = projected_gradient(
+            f=self._mle_objective, x0=params0, x=x, lr=lr, maxiter=maxiter, 
+            projection_method='projection_non_negative')
         
         alpha, beta = res["x"]
         return self._params_dict(alpha=alpha, beta=beta)#, res["fun"]
     
-    def fit(self, x: ArrayLike, *args, **kwargs) -> dict:
+    def fit(self, x: ArrayLike, lr: float, maxiter: int) -> dict:
         x: jnp.ndarray = _univariate_input(x)[0]
-        return self._fit_mle(x=x, *args, **kwargs)
+        return self._fit_mle(x=x, lr=lr, maxiter=maxiter)
     
 
 ig = IG("IG")
