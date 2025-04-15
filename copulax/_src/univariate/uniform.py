d44701e1a956e0abef1252c7807618041ada2e7f
tfm000 <tylerfmitchell1999@gmail.com>
2025-04-15 16:45:10 +0100

    updating uniform

--- copulax/_src/univariate/uniform.py
+++ copulax/_src/univariate/uniform.py
@@ -23,75 +23,74 @@ class Uniform(Univariate):
     upper bound.
     """
     def _params_dict(self, a: Scalar, b: Scalar) -> dict:
-        a, b = self._args_transform(a, b)
-        return {"a": a, "b": b}
+        d: dict = {"a": a, "b": b}
+        return self._args_transform(d)
     
-    def support(self, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
-        a, b = self._args_transform(a, b)
+    def _params_to_tuple(self, params: dict) -> tuple:
+        params = self._args_transform(params)
+        return params["a"], params["b"]
+    
+    def example_params(self, *args, **kwargs) -> dict:
+        r"""Example parameters for the uniform distribution.
+        
+        This is a two parameter family, with the uniform being defined by 
+        its lower and upper bounds.
+        """
+        return self._params_dict(a=0.0, b=1.0)
+    
+    def support(self, params: dict) -> Array:
+        a, b = self._params_to_tuple(params)
         return jnp.array([a, b]).flatten()
 
-    def logpdf(self, x: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
+    def logpdf(self, x: ArrayLike, params: dict) -> Array:
         x, xshape = _univariate_input(x)
-        a, b = self._args_transform(a, b)
+        a, b = self._params_to_tuple(params)
 
         log_pdf: jnp.ndarray = -jnp.log(lax.sub(b, a))
         log_pdf = jnp.where(jnp.logical_and(x >= a, x <= b), log_pdf, -jnp.inf)
         return log_pdf.reshape(xshape)
     
-    def pdf(self, x: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
-        return super().pdf(x=x, a=a, b=b)
-    
-    def logcdf(self, x: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
+    def logcdf(self, x: ArrayLike, params : dict) -> Array:
         x, xshape = _univariate_input(x)
-        a, b = self._args_transform(a, b)
+        a, b = self._params_to_tuple(params)
 
         log_cdf: jnp.ndarray = jnp.log(lax.sub(x, a)) - jnp.log(lax.sub(b, a))
         log_cdf = jnp.where(jnp.logical_and(x >= a, x <= b), log_cdf, -jnp.inf)
         return log_cdf.reshape(xshape)
     
-    def cdf(self, x: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
-        return jnp.exp(self.logcdf(x=x, a=a, b=b))
+    def cdf(self, x: ArrayLike, params: dict) -> Array:
+        return jnp.exp(self.logcdf(x=x, params=params))
     
-    def ppf(self, q: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
+    # ppf
+    def _get_x0(self, params: dict) -> Scalar:
+        stats: dict = self.stats(params=params)
+        return stats["mean"]
+    
+    def _ppf(self, q: ArrayLike, params: dict) -> Array:
         q, qshape = _univariate_input(q)
-        a, b = self._args_transform(a, b)
+        a, b = self._params_to_tuple(params)
         
         ppf_values: jnp.ndarray = lax.add(a, lax.mul(q, lax.sub(b, a)))
         ppf_values = jnp.where(jnp.logical_and(q >= 0, q <= 1), ppf_values, jnp.nan)
         return ppf_values.reshape(qshape)
     
-    def inverse_cdf(self, q: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
-        return super().inverse_cdf(q=q, a=a, b=b)
-    
     # sampling
-    def rvs(self, size: tuple | Scalar = (), key=DEFAULT_RANDOM_KEY, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
-        a, b = self._args_transform(a, b)
+    def rvs(self, size: tuple | Scalar, params: dict, key=DEFAULT_RANDOM_KEY) -> Array:
+        a, b = self._params_to_tuple(params)
         return random.uniform(key=key, shape=size, minval=a, maxval=b)
     
-    def sample(self, size: tuple | Scalar = (), key=DEFAULT_RANDOM_KEY, a: Scalar = 0.0, b: Scalar = 1.0) -> Array:
-        return super().sample(size=size, key=key, a=a, b=b)
-    
     # stats
-    def stats(self, a: Scalar = 0.0, b: Scalar = 1.0) -> dict:
-        a, b = self._args_transform(a, b)
+    def stats(self, params: dict) -> dict:
+        a, b = self._params_to_tuple(params)
 
         mean: Scalar = (a + b) / 2
         variance: Scalar = lax.pow(b - a, 2) / 12
-        return {'mean': mean, 'median': mean, 'variance': variance, 
-                'skewness': 0.0, 'kurtosis': -6 / 5}
-    
-    # metrics
-    def loglikelihood(self, x, a: Scalar = 0.0, b: Scalar = 1.0) -> Scalar:
-        return super().loglikelihood(x, a=a, b=b)
-    
-    def aic(self, x, a: Scalar = 0.0, b: Scalar = 1.0) -> Scalar:
-        return super().aic(x, a=a, b=b)
-    
-    def bic(self, x, a: Scalar = 0.0, b: Scalar = 1.0) -> Scalar:
-        return super().bic(x, a=a, b=b)
+        std: Scalar = jnp.sqrt(variance)
+        return {'mean': mean, 'median': mean, 'variance': variance,
+                'std': std, 'skewness': 0.0, 'kurtosis': -6 / 5}
 
     # fitting
-    def fit(self, x: ArrayLike, a: Scalar = 0.0, b: Scalar = 1.0, *args, **kwargs) -> dict:
+    def fit(self, x: ArrayLike, *args, **kwargs) -> dict:
         x: jnp.ndarray = _univariate_input(x)[0]
         a: Scalar = jnp.min(x)
         b: Scalar = jnp.max(x)
