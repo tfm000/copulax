aa01d3b7dc8c24b4f6b9739e544a99db5e6f5b90
tfm000 <tylerfmitchell1999@gmail.com>
2025-06-04 19:00:06 +0100

    updating test helpers

--- copulax/tests/helpers.py
+++ copulax/tests/helpers.py
@@ -12,6 +12,26 @@ def jittable(dist):
     return dist.example_params()
 
 
+def check_uvt_params(params, s):
+    assert isinstance(params, dict), f"{s} params is not a dict"
+    assert len(params) > 0, f"{s} is empty"
+    assert all(isinstance(k, str) for k in params.keys()), f"{s} params keys are not strings"
+    assert all(isinstance(v, Scalar) for v in params.values()), f"{s} params values are not scalars"
+    assert all(v.ndim == 0 and v.shape == () and v.size == 1 for v in params.values()), f"{s} params values are not scalars"
+    assert any(jnp.isnan(v) for v in params.values()) == False, f"{s} params values are NaN"
+    assert all(jnp.isfinite(v) for v in params.values()) == True, f"{s} params values are not finite"
+
+
+def check_mvt_params(params, s):
+    assert isinstance(params, dict), f"{s} params is not a dict"
+    assert len(params) > 0, f"{s} is empty"
+    assert all(isinstance(k, str) for k in params.keys()), f"{s} params keys are not strings"
+    assert all(isinstance(v, jnp.ndarray) for v in params.values()), f"{s} params values are not arrays"
+    assert all((v.ndim == 0 and v.shape == () and v.size == 1) or (v.ndim == 2 and v.shape == (v.size, 1) and v.size > 1) or (v.ndim == 2 and v.shape == (int(v.size ** 0.5), int(v.size ** 0.5)) and v.size > 1) for v in params.values()), f"{s} params values are not scalars, 1D-vectors or 2D-square matrices"
+    assert any(jnp.any(jnp.isnan(v)) for v in params.values()) == False, f"{s} params values are NaN"
+    assert all(jnp.all(jnp.isfinite(v)) for v in params.values()) == True, f"{s} params values are not finite"
+
+
 def correct_uvt_shape(x, output, dist, method):
     """Check if the shape of the univariate output array is correct"""
     assert isinstance(output, jnp.ndarray), f"{method} output is not a JAX array for {dist}"
@@ -71,6 +91,7 @@ def gradients(func, s, data, params, params_error: bool = True, **kwargs):
     """Calculate the gradients of the output."""
     new_func = lambda x, p: func(x, params=p, **kwargs).sum()
     x_grad = grad(new_func, argnums=[0,])(data, params)
+    # breakpoint()
     # params_grad = tuple(params_grad.values())
     assert no_nans(x_grad), f"{s} gradient contains NaNs for data argument"
     assert is_finite(x_grad), f"{s} gradient contains non-finite values for data argument"
