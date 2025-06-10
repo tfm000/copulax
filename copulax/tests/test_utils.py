a5eca12efecb46c717bf2aaeb10993b1a75ac12b
tfm000 <tylerfmitchell1999@gmail.com>
2025-06-10 16:29:02 +0100

    updating bug in utils tests

--- copulax/tests/test_utils.py
+++ copulax/tests/test_utils.py
@@ -1,4 +1,5 @@
 from jax._src.prng import PRNGKeyArray
+import pytest
 
 from copulax import get_random_key, get_api_random_key, get_local_random_key
 
@@ -6,16 +7,37 @@ def _check_valid_key(key, s: str) -> None:
     """Checks if the key is a valid JAX PRNGKey."""
     assert isinstance(key, PRNGKeyArray), f"{s} key is not a valid JAX PRNGKey."
 
-def test_get_random_key():
-    """Tests the get_random_key function."""
-    # testing api method
-    api_key = get_api_random_key()
-    _check_valid_key(api_key, "API")
-    wrapper_api_key = get_random_key(local=False)
-    _check_valid_key(wrapper_api_key, "Wrapper API")
 
+def test_get_api_random_key():
+    # testing api method
+    try:
+        api_key = get_api_random_key()
+        _check_valid_key(api_key, "API")
+    except ConnectionError as e:
+        if str(e) == "Unable to connect to random.org after max = 10 attempts.":
+            pytest.skip("Skipping test due to connection error with random.org API.")
+        else:
+            pytest.fail(f"Failed to get API key: {e}")
+
+
+def test_wrapper_get_api_random_key():
+    try:
+        wrapper_api_key = get_random_key(local=False)
+        _check_valid_key(wrapper_api_key, "Wrapper API")
+    except ConnectionError as e:
+        if str(e) == "Unable to connect to random.org after max = 10 attempts.":
+            pytest.skip("Skipping test due to connection error with random.org API.")
+        else:
+            pytest.fail(f"Failed to get API key: {e}")
+
+
+def test_get_local_random_key():
     # testing local method
     local_key = get_local_random_key()
     _check_valid_key(local_key, "Local")
+
+
+def test_wrapper_get_local_random_key():
+    # testing wrapper local method
     wrapper_local_key = get_random_key(local=True)
     _check_valid_key(wrapper_local_key, "Wrapper Local")
