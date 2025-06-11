a164a655d47074e8a6d866bbc430fe175421352b
tfm000 <tylerfmitchell1999@gmail.com>
2025-06-11 12:50:37 +0100

    adding tests for random correlation and covariance

--- copulax/tests/multivariate/test_shape.py
+++ copulax/tests/multivariate/test_shape.py
@@ -36,6 +36,11 @@ def has_ones_on_diagonal(matrix, tol=1e-10):
     return jnp.allclose(diag, jnp.ones_like(diag), atol=tol)
 
 
+def is_bounded(matrix, tol=1e-5):
+    """Check if a matrix is bounded."""
+    return jnp.all(jnp.abs(matrix) <= 1 + tol)
+
+
 # Test methods
 CORRELATION_METHODS = [
     "pearson", "spearman", "kendall", "pp_kendall", 
@@ -48,11 +53,13 @@ CORRELATION_METHODS = [
 @pytest.mark.parametrize("method", CORRELATION_METHODS)
 def test_corr_on_correlated_data(correlated_sample, method):
     correlation = corr(correlated_sample, method=method)
+
     # Check properties
     assert is_square(correlation), f"{method} correlation matrix is not square"
     assert is_symmetric(correlation), f"{method} correlation matrix is not symmetric"
     assert has_ones_on_diagonal(correlation), f"{method} correlation matrix does not have ones on diagonal"
     assert is_positive_semi_definite(correlation), f"{method} correlation matrix is not positive semi-definite"
+    assert is_bounded(correlation), f"{method} correlation matrix is not bounded"
 
     # Checking works with jit
     jit(corr, static_argnames=("method",))(correlated_sample, method)
@@ -66,9 +73,11 @@ def test_corr_on_uncorrelated_data(uncorrelated_sample, method):
     assert is_symmetric(correlation), f"{method} correlation matrix is not symmetric with uncorrelated data"
     assert has_ones_on_diagonal(correlation), f"{method} correlation matrix does not have ones on diagonal with uncorrelated data"
     assert is_positive_semi_definite(correlation), f"{method} correlation matrix is not positive semi-definite with uncorrelated data"
+    assert is_bounded(correlation), f"{method} correlation matrix is not bounded with uncorrelated data"
 
     # Checking works with jit
     jit(corr, static_argnames=("method",))(uncorrelated_sample, method)
+
 
 # Tests for covariance matrices
 @pytest.mark.parametrize("method", CORRELATION_METHODS)
@@ -92,6 +101,42 @@ def test_cov_on_uncorrelated_data(uncorrelated_sample, method):
 
     # Checking works with jit
     jit(cov, static_argnames=("method",))(uncorrelated_sample, method)
+
+
+sizes = tuple((2, 3, 5))
+
+@pytest.mark.parametrize("n", sizes)
+def test_random_correlation(n):
+    """Test random correlation matrix generation."""
+    random_corr = corr.random_correlation(n)
+    
+    # Check properties
+    assert is_square(random_corr), "Random correlation matrix is not square"
+    assert random_corr.shape == (n, n), "Random correlation matrix has incorrect shape"
+    assert is_symmetric(random_corr), "Random correlation matrix is not symmetric"
+    assert has_ones_on_diagonal(random_corr), "Random correlation matrix does not have ones on diagonal"
+    assert is_positive_definite(random_corr), "Random correlation matrix is not positive definite"
+    assert is_bounded(random_corr), "Random correlation matrix is not bounded"
+
+    # Checking works with jit
+    jit(corr.random_correlation, static_argnames=("size",))(n)
+
+
+@pytest.mark.parametrize("n", sizes)
+def test_random_covariance(n):
+    """Test random covariance matrix generation."""
+    random_vars = np.random.uniform(size=(n, 1))
+    random_cov = cov.random_covariance(random_vars)
+    
+    # Check properties
+    assert is_square(random_cov), "Random covariance matrix is not square"
+    assert random_cov.shape == (n, n), "Random covariance matrix has incorrect shape"
+    assert is_symmetric(random_cov), "Random covariance matrix is not symmetric"
+    assert is_positive_definite(random_cov), "Random covariance matrix is not positive definite"
+
+    # Checking works with jit
+    jit(cov.random_covariance)(random_vars)
+
 
 # # Edge case tests
 # def test_corr_small_sample(correlated_small_sample):
