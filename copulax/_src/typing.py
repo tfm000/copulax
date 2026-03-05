b78b54445561f6fc7160ac2f0a402304d115c914
tfm000 <tylerfmitchell1999@gmail.com>
2026-03-05 21:28:45 +0000

    Refactor typing module: replace import from typing_extensions with typing for TypeAlias

--- copulax/_src/typing.py
+++ copulax/_src/typing.py
@@ -1,7 +1,7 @@
 """Custom type aliases for copulAX."""
 
 from typing import Union
-from typing_extensions import TypeAlias
+from typing import TypeAlias
 from jax.typing import ArrayLike
 
 Scalar = Union[float, int, ArrayLike]
