3d014f3e677c7ee03923b7f486a9ca987926f38f
tfm000 <tylerfmitchell1999@gmail.com>
2026-03-05 16:13:55 +0000

    Refactor copula and multivariate modules: consolidate distribution imports and create dedicated distribution files

--- copulax/copulas/__init__.py
+++ copulax/copulas/__init__.py
@@ -1,16 +1,3 @@
 """Copula distributions: elliptical and Archimedean families."""
 
-from copulax._src.copulas._distributions import (
-    gaussian_copula,
-    student_t_copula,
-    gh_copula,
-    skewed_t_copula,
-)
-from copulax._src.copulas._archimedean import (
-    clayton_copula,
-    frank_copula,
-    gumbel_copula,
-    joe_copula,
-    amh_copula,
-    independence_copula,
-)
+from copulax.copulas.distributions import *
